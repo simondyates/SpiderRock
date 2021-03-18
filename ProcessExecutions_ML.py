3cf3a9f907377247e22bbe89e390ecd22e2caf53
simondyates <simon.yates@gmail.com>
2021-03-18 11:23:17 -0400

    Bugfixes in ProcessExecutions_ML.py

--- ProcessExecutions_ML.py
+++ ProcessExecutions_ML.py
@@ -1,8 +1,69 @@
-## TO DO - clean up formatting on mleg saved files
 
 import pandas as pd
 from SRUtils import process_time_cols, format_df, make_title, find_first_file
 import os
+
+# Define the TCA datastructure as a global
+strng = ''
+comma = '{:>10,.0f}'
+price = '{:>10.2f}'
+pct0 = '{:>10.0%}'
+pct2 = '{:>10.2%}'
+rows_dict = {
+    'Order': (strng, ''),
+    'Arrival Mid': (price, 'Mid at first available time'),
+    'Arrival Mark': (price, 'SR Mark at first available time'),
+    'Arrival U Mid': (price, 'Mid of underlying at first available time'),
+    'Arrival Mid Vol': (pct2, 'Implied volatility of Arrival Mid at Arrival U Mid'),
+    'Arrival Mark Vol': (pct2, 'Implied volatility of Arrival Mark at Arrival U Mid'),
+    'Qwap': (price, 'SR-calculated Qwap (or Vwap for a stock only order)'),
+    'Qwap U': (price, 'SR-calculated Qwap for underlying price'),
+    'Qwap Vol': (pct2, 'Implied volatility of Qwap at Qwap U'),
+    'Delta': (pct0, 'Option Contract Delta'),
+    'Vega': (price, 'Option Contract Vega'),
+    'Child Orders': (comma, 'Number of child orders which had fills'),
+    'Avg Child Size': (comma, 'Avg size of child orders which had fills'),
+    'Filled Ctr': (comma, 'Total number of contracts filled'),
+    'Ctr Fill Rate': (pct0, 'Filled Contracts divided by total size sent by child orders which had fills'),
+    'Avg Fill Pct Spread': (pct2, '0% means fill is on bid at fill time; 100% means offer'),
+    'Exec Px': (price, 'Average filled price'),
+    'Px Range': (price, 'High minus low fill price'),
+    'Slip Arr Mid Px': (price, 'Amount by which Exec Px was more favorable than mid at order creation'),
+    'Slip Arr Mid USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Slip Arr Mark Px': (price, 'Amount by which Exec Px was more favorable than SR mark at order creation'),
+    'Slip Arr Mark USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Slip Qwap Px': (price, 'Amount by which Exec Px was more favorable than Qwap'),
+    'Slip Qwap USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Theo U Mid': (price, 'Average underlying price if hedging mid-market each fill time'),
+    'Exec DTheo Arr Mid Px': (price, 'Exec Px delta-adjusted from Theo U Mid to Arrival Mid'),
+    'DTheo Px Range': (price, 'High minus low delta-adjusted fill price'),
+    'DTheo Slip Arr Mid Px': (price, 'Amount by which Exec DTheo Arr Mid Px was more favorable than Arrival Mid'),
+    'DTheo Slip Arr Mid USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'DTheo Slip Arr Mark Px': (price, 'Amount by which Exec DTheo Arr Mid Px was more favorable than Arrival Mark'),
+    'DTheo Slip Arr Mark USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Exec DTheo Qwap Px': (price, 'Exec Px delta-adjusted from Theo U Mid to Qwap U'),
+    'DTheo Slip Qwap Px': (price, 'Amount by which Exec DTheo Qwap Px was more favorable than Qwap'),
+    'DTheo Slip Qwap USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Exec DTheo Vol': (pct2, 'Implied volatility of Exec DTheo Arr Mid Px at Arrival Mid'),
+    'DTheo Vol Range': (pct2, 'High minus low vol'),
+    'DTheo Slip Arr Mid Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mid Px at Arrival Mid'),
+    'DTheo Slip Arr Mark Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mark Px at Arrival Mid'),
+    'DTheo Slip Qwap Vol': (pct2, 'Implied volatility of DTheo Slip Qwap Px at Qwap U'),
+    'Act U Mid': (price, 'Actual average underlying price from executed hedge'),
+    'Exec DAct Arr Mid Px': (price, 'Exec Px delta-adjusted from Act U Mid to Arrival Mid'),
+    'DAct Slip Arr Mid Px': (price, 'Amount by which Exec DAct Arr Mid Px was more favorable than Arrival Mid'),
+    'DAct Slip Arr Mid USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'DAct Slip Arr Mark Px': (price, 'Amount by which Exec DAct Arr Mid Px was more favorable than Arrival Mark'),
+    'DAct Slip Arr Mark USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Exec DAct Qwap Px': (price, 'Exec Px delta-adjusted from Act U Mid to Qwap U'),
+    'DAct Slip Qwap Px': (price, 'Amount by which Exec DAct Qwap Px was more favorable than Qwap'),
+    'DActSlip Qwap USD': (comma, 'Above  * contracts filled * contract multiplier'),
+    'Exec DAct Vol': (pct2, 'Implied volatility of Exec DAct Arr Mid Px at Arrival Mid'),
+    'DAct Slip Arr Mid Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mid Px at Arrival Mid'),
+    'DAct Slip Arr Mark Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mark Px at Arrival Mid'),
+    'DAct Slip Qwap Vol': (pct2, 'Implied volatility of DTheo Slip Qwap Px at Qwap U')}
+
+format_dict = {key: rows_dict[key][0] for key in rows_dict.keys()}
 
 def calc_TCA_metrics(df, qwap=None, qwapU=None, arrActSlipPct=None, formatted=True):
     """Returns a dataframe of TCA metrics for an option or stock order on SpiderRock
@@ -33,65 +94,6 @@ def calc_TCA_metrics(df, qwap=None, qwapU=None, arrActSlipPct=None, formatted=Tr
 
     # Build results df so as to put variable definitions at start
     cols = ['Maker', 'Taker', 'Total', 'Desc']
-    strng = ''
-    comma = '{:>10,.0f}'
-    price = '{:>10.2f}'
-    pct0 = '{:>10.0%}'
-    pct2 = '{:>10.2%}'
-    rows_dict = {
-        'Order': (strng, make_title(df)),
-        'Arrival Mid': (price, 'Mid at first available time'),
-        'Arrival Mark': (price, 'SR Mark at first available time'),
-        'Arrival U Mid': (price, 'Mid of underlying at first available time'),
-        'Arrival Mid Vol': (pct2, 'Implied volatility of Arrival Mid at Arrival U Mid'),
-        'Arrival Mark Vol': (pct2, 'Implied volatility of Arrival Mark at Arrival U Mid'),
-        'Qwap': (price, 'SR-calculated Qwap (or Vwap for a stock only order)'),
-        'Qwap U': (price, 'SR-calculated Qwap for underlying price'),
-        'Qwap Vol': (pct2, 'Implied volatility of Qwap at Qwap U'),
-        'Delta': (pct0, 'Option Contract Delta'),
-        'Vega': (price, 'Option Contract Vega'),
-        'Child Orders': (comma, 'Number of child orders which had fills'),
-        'Avg Child Size': (comma, 'Avg size of child orders which had fills'),
-        'Filled Ctr': (comma, 'Total number of contracts filled'),
-        'Ctr Fill Rate': (pct0, 'Filled Contracts divided by total size sent by child orders which had fills'),
-        'Avg Fill Pct Spread': (pct2, '0% means fill is on bid at fill time; 100% means offer'),
-        'Exec Px': (price, 'Average filled price'),
-        'Px Range': (price, 'High minus low fill price'),
-        'Slip Arr Mid Px': (price, 'Amount by which Exec Px was more favorable than mid at order creation'),
-        'Slip Arr Mid USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Slip Arr Mark Px': (price, 'Amount by which Exec Px was more favorable than SR mark at order creation'),
-        'Slip Arr Mark USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Slip Qwap Px': (price, 'Amount by which Exec Px was more favorable than Qwap'),
-        'Slip Qwap USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Theo U Mid': (price, 'Average underlying price if hedging mid-market each fill time'),
-        'Exec DTheo Arr Mid Px': (price, 'Exec Px delta-adjusted from Theo U Mid to Arrival Mid'),
-        'DTheo Px Range': (price, 'High minus low delta-adjusted fill price'),
-        'DTheo Slip Arr Mid Px': (price, 'Amount by which Exec DTheo Arr Mid Px was more favorable than Arrival Mid'),
-        'DTheo Slip Arr Mid USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'DTheo Slip Arr Mark Px': (price, 'Amount by which Exec DTheo Arr Mid Px was more favorable than Arrival Mark'),
-        'DTheo Slip Arr Mark USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Exec DTheo Qwap Px': (price, 'Exec Px delta-adjusted from Theo U Mid to Qwap U'),
-        'DTheo Slip Qwap Px': (price, 'Amount by which Exec DTheo Qwap Px was more favorable than Qwap'),
-        'DTheo Slip Qwap USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Exec DTheo Vol': (pct2, 'Implied volatility of Exec DTheo Arr Mid Px at Arrival Mid'),
-        'DTheo Vol Range': (pct2, 'High minus low vol'),
-        'DTheo Slip Arr Mid Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mid Px at Arrival Mid'),
-        'DTheo Slip Arr Mark Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mark Px at Arrival Mid'),
-        'DTheo Slip Qwap Vol': (pct2, 'Implied volatility of DTheo Slip Qwap Px at Qwap U'),
-        'Act U Mid': (price, 'Actual average underlying price from executed hedge'),
-        'Exec DAct Arr Mid Px': (price, 'Exec Px delta-adjusted from Act U Mid to Arrival Mid'),
-        'DAct Slip Arr Mid Px': (price, 'Amount by which Exec DAct Arr Mid Px was more favorable than Arrival Mid'),
-        'DAct Slip Arr Mid USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'DAct Slip Arr Mark Px': (price, 'Amount by which Exec DAct Arr Mid Px was more favorable than Arrival Mark'),
-        'DAct Slip Arr Mark USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Exec DAct Qwap Px': (price, 'Exec Px delta-adjusted from Act U Mid to Qwap U'),
-        'DAct Slip Qwap Px': (price, 'Amount by which Exec DAct Qwap Px was more favorable than Qwap'),
-        'DActSlip Qwap USD': (comma, 'Above  * contracts filled * contract multiplier'),
-        'Exec DAct Vol': (pct2, 'Implied volatility of Exec DAct Arr Mid Px at Arrival Mid'),
-        'DAct Slip Arr Mid Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mid Px at Arrival Mid'),
-        'DAct Slip Arr Mark Vol': (pct2, 'Implied volatility of DTheo Slip Arr Mark Px at Arrival Mid'),
-        'DAct Slip Qwap Vol': (pct2, 'Implied volatility of DTheo Slip Qwap Px at Qwap U')}
-
     results = pd.DataFrame(index=rows_dict.keys(), columns=cols)
 
     # Restrict calculations to positive quantity fills only
@@ -292,11 +294,11 @@ def calc_TCA_metrics(df, qwap=None, qwapU=None, arrActSlipPct=None, formatted=Tr
     # Add descriptions
     for key in rows_dict.keys():
         results.loc[key, 'Desc'] = rows_dict[key][1]
+    results.loc['Order', 'Desc'] = make_title(df)
 
     # Add formatting and return results
     if formatted:
-        format_dict = {key: rows_dict[key][0] for key in rows_dict.keys()}
-        results = format_df(results, format_dict)
+       results = format_df(results, format_dict)
     return results
 
 def process_day_TCA(dt):
@@ -326,7 +328,6 @@ def process_day_TCA(dt):
         return wins
 
     for grp in dayFills['riskGroupId'].unique():
-        print(grp)
         parents = dayFills.loc[dayFills['riskGroupId'] == grp, 'baseParentNumber'].unique()
         opt_parents = [p for p in parents if dayFills.loc[dayFills['baseParentNumber'] == p, 'secType'].iloc[0] == 'Option']
         stock_parents = [p for p in parents if dayFills.loc[dayFills['baseParentNumber'] == p, 'secType'].iloc[0] == 'Stock']
@@ -341,7 +342,7 @@ def process_day_TCA(dt):
             else:
                 arrActSlipPct = None
             for opt in opt_parents:
-                fills = dayFills[dayFills['baseParentNumber'] == opt]
+                fills = dayFills[dayFills['baseParentNumber'] == opt].copy()
                 qwap = qwapU = None
                 if fills.loc[fills.index[0], 'execShape'] == 'Single':
                     # Look for qwap data matching opt
@@ -366,21 +367,13 @@ def process_day_TCA(dt):
                             s += f'{l[4]:.2f} '
                         s += l[5]
                         return s
+
                     keyCols = ['secKey_tk',
                             'secKey_yr', 'secKey_mn', 'secKey_dy',
                             'secKey_xx',
                             'secKey_cp']
                     fills['optName'] = fills[keyCols].apply(get_opt_name, axis=1)
-                    result_dict = {leg: {'side': '', 'qty': 0, 'results': None} for leg in fills['optName'].unique()}
-                    for i, leg in enumerate(result_dict.keys()):
-                        legFills = fills[fills['optName'] == leg]
-                        results = calc_TCA_metrics(legFills, None, None, arrActSlipPct, False)
-                        fName = f'{dt:%Y%m%d} {opt}-{i+1}.csv'
-                        results.to_csv(os.path.join(os.getcwd(), 'TCA', fName))
-                        wins += 1
-                        result_dict[leg] = {'side': legFills.loc[legFills.index[0], 'orderSide'],
-                                            'qty': legFills['fillQuantity'].sum(),
-                                            'results': results}
+
                     opt_str = ''
                     sum_results = None
                     no_sum = ['Order',
@@ -395,26 +388,36 @@ def process_day_TCA(dt):
                               'DTheo Vol Range',
                               'Act U Mid']
                     min_qty = float('inf')
-                    for k in result_dict.keys():
-                        qty = result_dict[k]['qty']
+
+                    for i, leg in enumerate(fills['optName'].unique()):
+                        legFills = fills[fills['optName'] == leg]
+                        results = calc_TCA_metrics(legFills, None, None, arrActSlipPct, False)
+                        # Do summing here
+                        qty = results.loc['Filled Ctr', 'Total']
                         if 0 < qty < min_qty:
                             min_qty = qty
-                        if result_dict[k]['side'] == 'Buy':
+                        if legFills.loc[legFills.index[0], 'orderSide'] == 'Buy':
                             mult = qty
                         else:
                             mult = -qty
-                        opt_str += k + ' '
-                        df = result_dict[k]['results']
+                        opt_str += make_title(legFills) + ' '
                         if sum_results is None:
-                            sum_results = df.copy()
-                            sum_results.loc[~df.index.isin(no_sum), ['Maker', 'Taker', 'Total']] = 0
+                            sum_results = results.copy()
+                            sum_results.loc[~sum_results.index.isin(no_sum), ['Maker', 'Taker', 'Total']] = 0
 
-                        sum_results.loc[~df.index.isin(no_sum), ['Maker', 'Taker', 'Total']] += \
-                            df.loc[~df.index.isin(no_sum), ['Maker', 'Taker', 'Total']] * mult
+                        sum_results.loc[~sum_results.index.isin(no_sum), ['Maker', 'Taker', 'Total']] += \
+                            results.loc[~sum_results.index.isin(no_sum), ['Maker', 'Taker', 'Total']] * mult
 
-                    sum_results.loc[~df.index.isin(no_sum), ['Maker', 'Taker', 'Total']] /= min_qty
+                        # Then format and save
+                        fName = f'{dt:%Y%m%d} {opt % 100000}-{i+1}.csv'
+                        results = format_df(results, format_dict)
+                        results.to_csv(os.path.join(os.getcwd(), 'TCA', fName))
+                        wins += 1
+
+                    sum_results.loc[~sum_results.index.isin(no_sum), ['Maker', 'Taker', 'Total']] /= min_qty
                     sum_results.iloc[0, 3] = opt_str
-                    fName = f'{dt:%Y%m%d} {opt}-Cons.csv'
+                    fName = f'{dt:%Y%m%d} {opt % 100000}-Cons.csv'
+                    sum_results = format_df(sum_results, format_dict)
                     sum_results.to_csv(os.path.join(os.getcwd(), 'TCA', fName))
                     wins += 1
 
@@ -430,7 +433,7 @@ def process_day_TCA(dt):
                         qwap = brkr.loc[brkr.index[0], 'brokerVwapMark']
                 fills = dayFills[dayFills['baseParentNumber'] == stock]
                 results = calc_TCA_metrics(fills, qwap)
-                fName = f'{dt:%Y%m%d} {stock}.csv'
+                fName = f'{dt:%Y%m%d} {stock % 100000}.csv'
                 results.to_csv(os.path.join(os.getcwd(), 'TCA', fName))
                 wins += 1
 
